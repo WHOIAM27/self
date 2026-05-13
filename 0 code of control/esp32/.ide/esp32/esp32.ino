103ec6d99938ac824540c2ba35db8a7eafa5f5b9
WHOIAM27 <rohittodkar92@gmail.com>
2026-05-13 09:52:49 +0530

    Update esp32.ino

--- 0 code of control/esp32/.ide/esp32/esp32.ino
+++ 0 code of control/esp32/.ide/esp32/esp32.ino
@@ -3,6 +3,15 @@
 #include <Adafruit_SSD1306.h>
 #include <WiFi.h>
 #include <WebSocketsServer.h> 
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define BLE_SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define BLE_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+bool bleConnected = false;
 
 // --- OLED Display Settings ---
 #define SCREEN_WIDTH 128
@@ -35,6 +44,72 @@ float pitchOffset = 0.0;
 int turnOffsetVal = 0;
 
 unsigned long lastUpdate = 0;
+String serialBuffer = "";
+
+void processIncomingMessage(String msg) {
+  if (msg.startsWith("BAL:")) {
+    if (msg.substring(4) == "ON") {
+      isSystemOn = true;
+    } else {
+      isSystemOn = false;
+      currentMode = "STANDBY";
+      directionStr = "STOP";
+    }
+  }
+  else if (msg.startsWith("MODE:")) {
+    currentMode = msg.substring(5);
+  }
+  else if (msg.startsWith("ARR:")) {
+    int commaIdx = msg.indexOf(',');
+    if (commaIdx > 0) {
+      int x = msg.substring(4, commaIdx).toInt();
+      int y = msg.substring(commaIdx + 1).toInt();
+      
+      pitchOffset = y * (1.5 / 100.0); 
+      turnOffsetVal = x * (30.0 / 100.0); 
+
+      if (abs(y) > abs(x)) directionStr = (y > 0) ? "FWD" : "REV";
+      else if (abs(x) > 0) directionStr = (x > 0) ? "RIGHT" : "LEFT";
+      else directionStr = "STOP";
+    }
+  }
+  else if (msg.startsWith("SLI:")) {
+    int speed = msg.substring(4).toInt();
+    pitchOffset = speed * (1.5 / 100.0);
+    turnOffsetVal = 0;
+    
+    if (speed == 0) directionStr = "STOP";
+    else if (speed > 0) directionStr = "FWD " + String(speed) + "%";
+    else directionStr = "REV " + String(abs(speed)) + "%";
+  }
+  else if (msg.startsWith("GYRO:")) {
+    int commaIdx = msg.indexOf(',');
+    if (commaIdx > 0) {
+      int x = msg.substring(5, commaIdx).toInt();
+      int y = msg.substring(commaIdx + 1).toInt();
+      pitchOffset = y * (1.5 / 100.0); 
+      turnOffsetVal = x * (30.0 / 100.0); 
+      directionStr = "TILT";
+    }
+  }
+}
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) { bleConnected = true; }
+    void onDisconnect(BLEServer* pServer) { 
+      bleConnected = false; 
+      pServer->getAdvertising()->start(); 
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      String rxValue = pCharacteristic->getValue().c_str();
+      if (rxValue.length() > 0) {
+        processIncomingMessage(rxValue);
+      }
+    }
+};
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
@@ -43,57 +118,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       break;
     case WStype_TEXT:
       String msg = String((char*)payload);
-      
-      if (msg.startsWith("BAL:")) {
-        if (msg.substring(4) == "ON") {
-          isSystemOn = true;
-        } else {
-          isSystemOn = false;
-          currentMode = "STANDBY";
-          directionStr = "STOP";
-        }
-      }
-      else if (msg.startsWith("MODE:")) {
-        currentMode = msg.substring(5);
-      }
-      else if (msg.startsWith("ARR:")) {
-        int commaIdx = msg.indexOf(',');
-        if (commaIdx > 0) {
-          int x = msg.substring(4, commaIdx).toInt();
-          int y = msg.substring(commaIdx + 1).toInt();
-          
-          pitchOffset = y * (1.5 / 100.0); // Reduced to 1.5 deg max lean so it doesn't fall
-          turnOffsetVal = x * (30.0 / 100.0); // Reduced turn speed
-
-          // Update OLED String based on dominant axis
-          if (abs(y) > abs(x)) {
-            directionStr = (y > 0) ? "FWD" : "REV";
-          } else if (abs(x) > 0) {
-            directionStr = (x > 0) ? "RIGHT" : "LEFT";
-          } else {
-            directionStr = "STOP";
-          }
-        }
-      }
-      else if (msg.startsWith("SLI:")) {
-        int speed = msg.substring(4).toInt();
-        pitchOffset = speed * (1.5 / 100.0);
-        turnOffsetVal = 0;
-        
-        if (speed == 0) directionStr = "STOP";
-        else if (speed > 0) directionStr = "FWD " + String(speed) + "%";
-        else directionStr = "REV " + String(abs(speed)) + "%";
-      }
-      else if (msg.startsWith("GYRO:")) {
-        int commaIdx = msg.indexOf(',');
-        if (commaIdx > 0) {
-          int x = msg.substring(5, commaIdx).toInt();
-          int y = msg.substring(commaIdx + 1).toInt();
-          pitchOffset = y * (1.5 / 100.0); 
-          turnOffsetVal = x * (30.0 / 100.0); 
-          directionStr = "TILT";
-        }
-      }
+      processIncomingMessage(msg);
       break;
   }
 }
@@ -123,19 +148,39 @@ void setup() {
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
   
+  // Setup BLE
+  BLEDevice::init("AeroBalance");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(BLE_SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         BLE_CHARACTERISTIC_UUID,
+                                         BLECharacteristicProperty::PROPERTY_READ |
+                                         BLECharacteristicProperty::PROPERTY_WRITE |
+                                         BLECharacteristicProperty::PROPERTY_NOTIFY
+                                       );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+  pServer->getAdvertising()->start();
+
   delay(1500); 
 }
 
 void loop() {
   webSocket.loop();
 
-  // --- Read incoming Tilt Angle from Arduino UNO ---
+  // --- Read incoming Tilt Angle non-blocking ---
   while (Serial2.available()) {
-    String incomingData = Serial2.readStringUntil('\n');
-    incomingData.trim(); // Remove whitespace/newlines
-    
-    if (incomingData.startsWith("T:")) {
-      tiltAngle = incomingData.substring(2); // Extract just the number
+    char c = Serial2.read();
+    if (c == '\n') {
+      serialBuffer.trim();
+      if (serialBuffer.startsWith("T:")) {
+        tiltAngle = serialBuffer.substring(2); 
+      }
+      serialBuffer = "";
+    } else {
+      serialBuffer += c;
     }
   }
 
@@ -198,7 +243,7 @@ void loop() {
     display.setTextColor(SSD1306_BLACK);
     display.setTextSize(1);
     display.setCursor(2, 2);
-    if (webSocket.connectedClients() > 0) {
+    if (webSocket.connectedClients() > 0 || bleConnected) {
       display.print("[<->] "); 
     } else {
       display.print("[ x ] "); 
